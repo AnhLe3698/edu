ea4179ac2b9fd08869f02b85a6826f33786cd657
Anhle3698 <anhviet.le00@hotmail.com>
2023-07-18 20:19:10 -0600

    Add initial callback from server to Frontend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(cors()); // This is new
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
