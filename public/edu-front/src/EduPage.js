306b9de61a1c8eae1ba01e35daee9017c1f88005
AnhLe3698 <anhviet.le00@hotmail.com>
2023-08-04 03:24:35 -0600

    Change style

--- public/edu-front/src/EduPage.js
+++ public/edu-front/src/EduPage.js
@@ -88,12 +88,31 @@ const QuestionsSection = ({ active }) => {
 const WikiEntriesSection = ({ active }) => (
   <section className={active ? 'active' : ''} id="wiki-entries">
       <h2>Wiki Entries</h2>
-      <h3>Kinematics: An Introduction</h3>
-      <p>Kinematics is the branch of physics that describes the motion of points, objects, and systems of bodies without considering the forces that caused the motion.</p>
-      <h3>Equations of Motion</h3>
-      <p>Equations of motion are mathematical formulas that describe how an object moves, taking into account its initial velocity, acceleration, and time traveled.</p>
-      {/* ... other content */}
+      <h1>Kinematics: An Introduction</h1>
+      <p>Kinematics is a subfield of physics that describes the motion of points, bodies (objects), and systems of bodies (groups of objects) without considering the forces that caused their motion. The term "kinematics" is derived from the Greek κίνημα (kinesis), meaning "motion."</p>
+
+      <h2>Key Concepts in Kinematics</h2>
+      <ul>
+          <li><strong>Displacement:</strong> Displacement is a vector quantity that represents the change in position of an object. Unlike distance, displacement is directional. Thus, it can be positive or negative.</li>
+          <li><strong>Velocity:</strong> Velocity is a vector quantity that measures the rate of change of the object's displacement. Like displacement, velocity has both magnitude (speed) and direction.</li>
+          <li><strong>Acceleration:</strong> Acceleration is another vector quantity that measures the rate of change of velocity. An object accelerates if it speeds up, slows down, or changes direction.</li>
+      </ul>
+
+      <h2>Kinematic Equations</h2>
+      <p>Kinematics is well-known for its "kinematic equations," which describe the mathematical relationship between displacement, velocity, acceleration, and time. These equations are used when acceleration is constant.</p>
+      <ul>
+          <li>First Equation: v = u + at</li>
+          <li>Second Equation: s = ut + 1/2(at^2)</li>
+          <li>Third Equation: v^2 = u^2 + 2as</li>
+      </ul>
+
+      <h2>1-D, 2-D, and 3-D Kinematics</h2>
+      <p>Kinematics can be studied in one, two, or three dimensions. In one-dimensional kinematics, motion occurs along a single line. In two dimensions, motion happens in a plane. In three dimensions, objects move in space.</p>
+
+      <h2>Applications of Kinematics</h2>
+      <p>Kinematics has numerous applications, including engineering, video game and film animation, robotics, biomechanics, and astrophysics. It's a crucial foundation for understanding more advanced concepts in physics.</p>
   </section>
+
 );
 
 const VideosSection = ({ active }) => (
